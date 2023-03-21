e581c3a135cc932fa42f023804c214cb98661d46
Hai Ming <haiming_liang@hotmail.com>
2023-03-21 11:48:38 +0800

    finished: haiming

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -8,7 +8,7 @@ import Alert from 'react-bootstrap/Alert'
 import moment from 'moment'
 
 const TodoList = props => {
-    const [todos, setTodos ] = useState([])
+    const [todos, setTodos] = useState([])
 
     useEffect(() => {
         retrieveTodos();
@@ -19,15 +19,35 @@ const TodoList = props => {
             .then(response => {
                 setTodos(response.data)
             }).catch(e => {
-                console.log(e)
+            console.log(e)
+        })
+    }
+
+    const deleteTodo = (todoId) => {
+        TodoDataService.deleteTodo(todoId, props.token)
+            .then(response => {
+                retrieveTodos();
+            }).catch(e => {
+            console.log(e)
+        })
+    }
+
+    const completeTodo = (todoId) => {
+        TodoDataService.completeTodo(todoId, props.token)
+            .then(response => {
+                retrieveTodos();
+                console.log("complete Todo", todoId);
+            }).catch(e => {
+            console.log(e)
         })
     }
 
     return (
         <Container>
-            {props.token == null || props.token === "" ? (<Alert variant="warning">你未登录，请<Link to="/login">登录</Link>去查看你的待办事项.</Alert>): (
+            {props.token == null || props.token === "" ? (
+                <Alert variant="warning">你未登录，请<Link to="/login">登录</Link>去查看你的待办事项.</Alert>) : (
                 <div>
-                    <Link to="/todos/create">
+                    <Link to="/todos/create" state={{token: props.token}}>
                         <Button variant="online-info" className="mb-3">
                             添加待办事项
                         </Button>
@@ -36,14 +56,17 @@ const TodoList = props => {
                         return (
                             <Card key={todo.id} className='mb-3'>
                                 <Card.Body>
-                                    <div>
+                                    <div className={`${todo.completed ? "text-decoration-line-through" : ""}`}>
                                         <Card.Title>{todo.title}</Card.Title>
                                         <Card.Text><b>Memo:</b>{todo.memo}</Card.Text>
                                         <Card.Text>创建时间：{moment(todo.created).format("YYYY-MM-DD")}</Card.Text>
                                     </div>
-                                    <Link to={`/todos/${todo.id}`} state={  {currentTodo: todo, token: props.token}  }>
-                                        <Button variant="outline-info" className="me-2">编辑</Button> </Link>
-                                    <Button variant="outline-danger">删除</Button>
+                                    {!todo.completed &&
+                                        <Link to={`/todos/${todo.id}`} state={{currentTodo: todo, token: props.token}}>
+                                            <Button variant="outline-info" className="me-2">编辑</Button> </Link>}
+                                    <Button variant="outline-danger" onClick={() => deleteTodo(todo.id)}>删除</Button>
+                                    <Button variant="outline-success"
+                                            onClick={() => completeTodo(todo.id)}>完成</Button>
                                 </Card.Body>
                             </Card>
                         )
--- src/services/todo.js
+++ src/services/todo.js
@@ -13,7 +13,7 @@ class TodoDataServices{
 
     createTodo(data, token){
         axios.defaults.headers.common["Authorization"] = "Token " + token;
-        return axios.post("http://localhost:8000/api/todos/", data);
+        return axios.post("http://localhost:8000/api/todos", data);
     }
 
     deleteTodo(id, token){
@@ -23,7 +23,7 @@ class TodoDataServices{
 
     completeTodo(id, token){
         axios.defaults.headers.common["Authorization"] = "Token " + token;
-        return axios.put(`http://localhost:8000/api/todo/${id}/complete`);
+        return axios.put(`http://localhost:8000/api/todos/${id}/complete`);
     }
 
     login(data){
